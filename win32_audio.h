e6db1ff9ba89f80402d1793d2b64a99e6a011bcb
Jose M Rico <jmrico01@gmail.com>
2019-09-04 22:54:50 -0400

    tabs for real

--- win32_audio.h
+++ win32_audio.h
@@ -10,30 +10,30 @@
 
 enum AudioFormat
 {
-    AUDIO_FORMAT_PCM_INT16,
-    AUDIO_FORMAT_PCM_FLOAT32
+	AUDIO_FORMAT_PCM_INT16,
+	AUDIO_FORMAT_PCM_FLOAT32
 };
 
 struct Win32Audio
 {
-    IAudioClient* audioClient;
-    IAudioRenderClient* renderClient;
-    IAudioClock* audioClock;
-    AudioFormat format;
-    int bitsPerSample;
+	IAudioClient* audioClient;
+	IAudioRenderClient* renderClient;
+	IAudioClock* audioClock;
+	AudioFormat format;
+	int bitsPerSample;
 
-    int sampleRate;
-    int channels;
-    int bufferSizeSamples;
-    
-    int latency;
+	int sampleRate;
+	int channels;
+	int bufferSizeSamples;
+	
+	int latency;
 
-    bool32 midiInBusy;
-    MidiInput midiIn;
+	bool32 midiInBusy;
+	MidiInput midiIn;
 };
 
 bool32 Win32InitAudio(Win32Audio* winAudio, int bufferSizeMilliseconds);
 void Win32StopAudio(Win32Audio* winAudio);
 
 void Win32WriteAudioSamples(const Win32Audio* winAudio,
-    const GameAudio* gameAudio, int numSamples);
+	const GameAudio* gameAudio, int numSamples);
